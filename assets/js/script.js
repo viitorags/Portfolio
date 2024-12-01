b60550f05ab001cc0dde8840c0876677960835b0
viitorags <vitorgabriels1000@gmail.com>
2024-12-01 16:20:30 -0300

    Adicionando Arquivos!

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function checkVisibility() {
+    const elements = document.querySelectorAll('.about-content, .project-content, .contact-content');
+    
+    elements.forEach(element => {
+        const rect = element.getBoundingClientRect();
+        
+        // Se o elemento está 80% visível na tela
+        if (rect.top <= window.innerHeight * 0.8) {
+            element.classList.add('visible');
+        }
+    });
+}
+
+// Adiciona o evento de scroll para monitorar o movimento
+window.addEventListener('scroll', checkVisibility);
+
+// Chama a função uma vez para garantir que os elementos visíveis no carregamento sejam exibidos
+checkVisibility();
